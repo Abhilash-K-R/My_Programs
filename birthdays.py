8a0b865c7c084458a6756182e14e5c30fd0ab0a3
Abhilash K R <168660984+Abhilash-K-R@users.noreply.github.com>
2024-05-14 23:46:16 +0530

    Update birthdays.py

--- birthdays.py
+++ birthdays.py
@@ -6,11 +6,4 @@ while True:
     if name == '':
         break
 
-    if name in birthdays:
-        print(birthdays[name] + ' is the birthday of ' +name)
-    else:
-        print('I do not have birthday information for '+ name)
-        print('What is there birthday??')
-        bday=input()
-        birthdays[name]=bday
-        print('Birthday database successfully updated...')
+    
